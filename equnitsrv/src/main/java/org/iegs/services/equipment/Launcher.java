b73596922e3db3b81f093b09d4383939272f47f3
u5er <u5er@kae>
2017-10-11 19:24:05 +0300

    New Project equitsrv

---
+++ equnitsrv/src/main/java/org/iegs/services/equipment/Launcher.java
@@ -0,0 +1,7 @@
+package org.iegs.services.equipment;
+
+public class Launcher {
+    public static void main(String[] args) throws Exception {
+        System.out.println(" Hello World !!!");
+    }
+}
